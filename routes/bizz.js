993ed445a8b0d6e54460479c734966581817553d
harrison-symes <symeshjb@gmail.com>
2017-01-13 15:19:00 +1300

    minor updates to hivedata

--- routes/bizz.js
+++ routes/bizz.js
@@ -3,11 +3,17 @@ var router = express.Router();
 var bcrypt = require('bcrypt')
 var passport = require('../passport')
 const userDb = require('../db/userDb')
+const bizzDb = require('../db/bizzDb')
 
 /* GET users listing. */
 router.get('/home', ensureAuthenticated, function(req, res, next) {
   console.log(req.query.bizz_name);
-  res.json(req.query.bizz_name)
+  bizzDb.getBizzByName(req.query.bizz_name)
+    .then((response) => {
+      console.log({response[0]});
+      res.json(response[0])
+    })
+  // res.json(req.query.bizz_name)
 });
 
 // router.post('/login', (req, res) => {
